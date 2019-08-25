538ddc57c55bbb2eeb941f38679ffde4ba7ca825
XinyaoTian <leontian1024@gmail.com>
2019-08-25 18:22:01 +0800

    To docker

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ class Config(object):
 
     SECRET_KEY = os.environ.get('WEB_SERVER_SECRET_KEY') or 'abcdef020301abc8c86f'
 
-    MONGO_URI = "mongodb://dbuser:dbpassword@45.78.76.192:27017/testdb"
+    # MONGO_URI = "mongodb://dbuser:dbpassword@45.78.76.192:27017/testdb"
+    MONGO_URI = os.environ.get('MONGO_URI')
