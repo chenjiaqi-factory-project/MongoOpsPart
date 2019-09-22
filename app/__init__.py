e2724af63678ae54710cfa259a4f7292ac66b315
XinyaoTian <leontian1024@gmail.com>
2019-09-22 17:36:40 +0800

    添加用电量模块

--- app/__init__.py
+++ app/__init__.py
@@ -12,4 +12,8 @@ mongo = PyMongo(app)
 from app.water import bp as water_bp
 app.register_blueprint(water_bp, url_prefix='/api/water')
 
+# blueprint for water part
+from app.elec import bp as elec_bp
+app.register_blueprint(elec_bp, url_prefix='/api/elec')
+
 from app import routes, gas_api
